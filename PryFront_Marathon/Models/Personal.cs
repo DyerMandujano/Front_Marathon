85bb688db1ab5cba91ccb2e1fa9755490ccf55c9
DyerMandujano <dyer0206@gmail.com>
2024-11-04 20:58:13 -0500

    Agregar archivos de proyecto.

---
+++ PryFront_Marathon/Models/Personal.cs
@@ -0,0 +1,18 @@
+﻿namespace PryFront_Marathon.Models
+{
+    public class Personal
+    {
+        public string Dni_Personal { get; set; }
+        public int Cod_Perfil { get; set; }
+        public int Cod_Tipo_Trabajo { get; set; }
+        public int Cod_Local { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public DateOnly Fecha_Nac { get; set; }
+        public string Genero { get; set; }
+        public string User_Per { get; set; }
+        public string Contra_Per { get; set; }
+        public int Estado { get; set; }
+    }
+}
