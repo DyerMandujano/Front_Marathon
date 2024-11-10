1bc309519599fde4182b7a44e5f883868951993a
srood2812 <sroodvan@gmail.com>
2024-11-10 12:03:31 -0500

    Creación de clases para Front

---
+++ PryFront_Marathon/Models/Cod_Membresia.cs
@@ -0,0 +1,9 @@
+﻿namespace PryFront_Marathon.Models
+{
+    public class Cod_Membresia
+    {
+        public string Tipo_Membre { get; set; }
+        public int Puntos { get; set; }
+        public string Estado { get; set; }
+    }
+}
